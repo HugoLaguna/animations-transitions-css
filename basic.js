64e92fb9ee20ae531aca119d2a347967e896ecab
HugoLaguna <mkdrdre@gmail.com>
2018-08-04 16:38:03 -0500

    template basico para presentaciones

---
+++ basic.js
@@ -0,0 +1,30 @@
+var controlSlide = 0;
+document.addEventListener('keyup', (e) => {
+    selectSlide(e.key);
+  });
+
+function reclases( tagName, className ){
+    slides = byName(tagName);
+    for( i = 0 ; i < slides.length; i++){
+        slides[i].classList.remove(className);
+    }
+}
+function byName(stringName){
+    return document.getElementsByName(stringName);
+}
+
+function selectSlide(onkey){
+    if ( onkey=='ArrowLeft' || onkey=='ArrowRight' ){
+        if( controlSlide >= 0 ){
+            console.log(`keyup event. key property value is "${onkey}"`);
+            if( onkey=='ArrowLeft' && controlSlide > 0){
+                controlSlide--; 
+                reclases('sliders' , 'actived');  
+            }else if(onkey=='ArrowRight' &&  controlSlide < byName('sliders').length-1 ){
+                controlSlide++;
+                reclases('sliders' , 'actived');  
+            }
+            byName('sliders')[controlSlide].classList.add('actived');
+        }
+    }
+}
